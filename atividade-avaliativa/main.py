f08657eb7b24c0ae65439488f29c74105bc38ea2
Cayo Eduardo <cayo.neves@lifeapps.com.br>
2022-10-23 09:49:25 -0300

    Refatorando a classe e colocando retorno se o Aluno foi aprovado ou não

--- atividade-avaliativa/main.py
+++ atividade-avaliativa/main.py
@@ -5,7 +5,7 @@ Classe que representa o aluno com as suas 3 notas
 """
 
 
-class Aluno():
+class Aluno:
     """
         Representa um aluno
 
@@ -16,10 +16,11 @@ class Aluno():
 
     """
 
-    def __init__(self, nota1, nota2, nota3):
-        self._nota1 = nota1
-        self._nota2 = nota2
-        self._nota3 = nota3
+    def __init__(self, aluno_nota1, aluno_nota2, aluno_nota3):
+        self._nota1 = aluno_nota1
+        self._nota2 = aluno_nota2
+        self._nota3 = aluno_nota3
+        self._media = 0
 
     @property
     def nota1(self):
@@ -33,51 +34,74 @@ class Aluno():
     def nota3(self):
         return self._nota3
 
+    @property
+    def media(self):
+        return self._media
+
+    @media.setter
+    def media(self, media):
+        self._media = media
+
     def calcula_media(self):
         """
             Método calcula_media
 
-            Ele calcula as média das 3 notas que o aluno possui e imprime um boletim
+            Ele calcula as média das 3 notas que o aluno possui
             com as notas arredondadas e a média com somente duas casas decimais
         """
         media = round(((self.nota1 + self.nota2 + self.nota3) / 3), 2)
+        self.media = media
 
+    def mostrar_boletim(self):
+        """
+            Método mostrar_boletim
+
+            Ele imprime as notas do aluno, a média das notas e se o aluno foi aprovado ou não.
+            O aluno é aprovodo caso a nota seja maior ou igual a 5.
+        """
         print("\n------------ Boletim ------------")
         print("\n\tNota 1: {}".format(self.nota1))
         print("\tNota 2: {}".format(self.nota2))
         print("\tNota 3: {}".format(self.nota3))
 
-        print("\n\tA média do aluno é: {}".format(media))
+        print("\n\tA média do aluno é: {}".format(self.media))
+
+        if self.media >= 5:
+            print("\n\tAluno Aprovado")
+        else:
+            print("\n\tAluno Reprovado")
+
         print("---------------------------------")
 
 
-def inputNota(texto):
+def input_nota(texto):
     """
-    Método inputNota
+        Método input_nota
 
-    Esse método serve para pegar a nota que o usuário digitar e tratar possiveis erros
-    caso tenha algum erro, por exemplo digitar texto no campo de número
+        Esse método serve para pegar a nota que o usuário digitar e tratar possiveis erros
+        caso tenha algum erro, por exemplo digitar texto no campo de número
 
-    E faz um loop para perguntar a nota enquanto o usuário não digita uma nota válida
+        E faz um loop para perguntar a nota enquanto o usuário não digita uma nota válida
     """
     nota_formato = True
     nota = None
 
-    while (nota_formato):
+    while nota_formato:
         try:
             nota = float(input("Digite a " + texto + " nota do aluno: "))
             nota = round(nota)
             nota_formato = False
-        except ValueError as ve:
+        except ValueError:
             print('Você deve inserir um número. Ex.: (5 ou 7.5).')
 
     return nota
 
 
 if __name__ == '__main__':
-    nota1 = inputNota("primeira")
-    nota2 = inputNota("segunda")
-    nota3 = inputNota("terceira")
+    nota1 = input_nota("primeira")
+    nota2 = input_nota("segunda")
+    nota3 = input_nota("terceira")
 
     aluno = Aluno(nota1, nota2, nota3)
     aluno.calcula_media()
+    aluno.mostrar_boletim()
